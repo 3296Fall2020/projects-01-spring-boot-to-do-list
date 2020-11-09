0f0f6b551f491c1da4d33749d8386aba8dfd4de4
Jsndng408 <tuh22928@temple.edu>
2020-11-09 16:26:48 -0500

    Resolved login issues

--- frontend/src/components/userValidationComponents/signIn.js
+++ frontend/src/components/userValidationComponents/signIn.js
@@ -11,7 +11,7 @@ function makeRequest(email, password, history) {
 
     fetch('http://localhost:8080/user/login?email=' + email + "&password=" + password)
         .then((response) => {
-            if (response.status === 204) {
+            if (response.status === 200) {
                 console.log("successful log in");
                 history.push({
                     pathname: '/toDoList',
